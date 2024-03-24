0e2040d7163adce7b44e56780fe601ac78f871db
unknown <Muhammad Hassan Raza>
2024-03-24 16:39:29 +0500

    JavaScript Assignment 06

---
+++ app.js
@@ -0,0 +1,73 @@
+//Q1 
+var userName = prompt("Enter your name")
+alert('🌟 Welcome ' + ' ' +userName)
+document.write(`Name: ${userName}`)
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+//
+var val = prompt("Enter Your City Name")
+if(val){
+    document.write(`city: ${val}`)
+}
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+var user_val = prompt("Enter Your Favourite Fruit Name")
+if(user_val){
+    document.write(`Favourite Fruit: ${user_val}`)
+}
+
+document.write(`<br>`)
+document.write(`<br>`)
+document.write(`<br>`)
+
+//Q1
+var num1 = 5; num2 = 5;
+var result = num1+num2
+document.write('Result The value of 5 + 5 is:'+ ' '+result)
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+
+//Q2
+var a_1 = 20; b_2 = 10;
+var result = a_1-b_2
+document.write('Result The value of 20 - 10 is:'+ ' '+result)
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+//Q2
+var num_a = 2; num_b = 5;
+var result = num_a*num_b
+document.write('Result The value of 2 * 5 is:'+ ' '+result)
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+
+//Q1 
+var value_a = 11
+var val_result = --value_a + 1
+document.write('The value of --a is:'+ ' '+result)
+document.write(`<br>`)
+document.write('Now The value of --a is:'+ ' '+result)
+
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+//Q1
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+document.write('a is =' + '1')
+document.write(`<br>`)
+document.write('b is =' + '2')
+document.write(`<br>`)
+document.write('result is :' + result)
+
+
